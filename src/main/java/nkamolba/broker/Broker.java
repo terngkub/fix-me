de99615d34a8b64ecb6d5d6adb4e86e97e275b9c
terngkub <terngnattapol@gmail.com>
2020-02-03 13:33:33 +0100

    Add simple broker console

--- src/main/java/nkamolba/broker/Broker.java
+++ src/main/java/nkamolba/broker/Broker.java
@@ -7,11 +7,22 @@ public class Broker {
     static String ip = "localhost";
     static String id;
 
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(ip, port)) {
+            getUniqueID(socket);
+            runConsole(socket);
+            
+        } catch (UnknownHostException e) {
+            System.out.println("Server not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("I/O error: " + e.getMessage());
+        }
+    }
+
     static void getUniqueID(Socket socket) {
         try {
             InputStream input = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
             String response = reader.readLine();
             id = response;
             System.out.println("Receive unique ID from the router: " + id);
@@ -21,14 +32,28 @@ public class Broker {
         }
     }
 
-    public static void main(String[] args) {
-        try (Socket socket = new Socket(ip, port)) {
-            getUniqueID(socket);
-        } catch (UnknownHostException e) {
-            System.out.println("Server not found: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("I/O error: " + e.getMessage());
+    static void runConsole(Socket socket) {
+        Console console = System.console();
+        while (true) {
+            String line = console.readLine("Enter command: ");
+
+            if (line.equals("exit")) {
+                break;
+            }
+            
+            try {
+                OutputStream output = socket. getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                writer.println(line);
+                System.out.println("Sent message to router: " + line);
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String response = reader.readLine();
+                System.out.println("Receive message from router: " + response);
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e.getMessage());
+            }
         }
     }
-
 }
--- src/main/java/nkamolba/router/BrokerTask.java
+++ src/main/java/nkamolba/router/BrokerTask.java
@@ -13,11 +13,26 @@ class BrokerTask implements Runnable {
     public void run() {
         System.out.println("New broker connected");
         try {
-            OutputStream output = socket. getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-            writer.println("Hello broker");
+            while (socket.isConnected()) {
+                
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                OutputStream output = socket. getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+                String response = reader.readLine();
+                if (response == null) {
+                    break;
+                }
+
+                System.out.println("Received message: " + response);
+
+                writer.println("Router received message: " + response);
+            }
         } catch (IOException e) {
-            
+            System.out.println("I/O error: " + e.getMessage());
+        } finally {
+            System.out.println("Connection close");
         }
     }
 
